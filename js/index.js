1f5f80f33775ee94630acfc5bf999eba15b10ecc
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2020-06-17 10:49:10 +0200

    test

--- js/index.js
+++ js/index.js
@@ -9,33 +9,41 @@ hamburger.addEventListener('click', () => {
 function animationTimeline() {
   const tl = gsap.timeline();
 
-  tl.to('.animated-social-media', {
-    opacity: 1,
-    duration: 1,
-    stagger: 0.5,
-  });
+  let hasPlayed = sessionStorage.getItem('hasAnimationPlayed');
 
-  /*   tl.from('.animated-heading', {
-    rotation: -180,
-    y: 200,
-    opacity: 0,
-    scale: 0.1,
-    duration: 1,
-    ease: 'back',
-    stagger: 1,
-  }); */
+  if (!hasPlayed) {
+    tl.from('.animated-social-media', {
+      opacity: 0,
+      duration: 1,
+      stagger: 0.5,
+      x: 50,
+      y: -50,
+      ease: 'sine',
+      onComplete: function () {
+        sessionStorage.setItem('hasAnimationPlayed', true);
+      },
+    });
 
-  tl.from('.image-container__image', {
-    opacity: 0,
-    duration: 1,
-    opacity: 0,
-    y: 50,
-    x: 150,
-    rotate: 120,
-    scale: 2,
-    duration: 3,
-    ease: 'back',
-  });
+    tl.from('.animated-heading', {
+      opacity: 0,
+
+      y: 250,
+      scale: 0.1,
+      duration: 1,
+      stagger: 1,
+      ease: 'back',
+    });
+
+    tl.from('.image-container__image', {
+      opacity: 0,
+      duration: 1.5,
+      rotation: 80,
+      scale: 1.8,
+      x: 20,
+      y: -50,
+      ease: 'back',
+    });
+  }
 }
 
 animationTimeline();
